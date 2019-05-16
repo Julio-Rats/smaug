ec357e731b0779f62771674afdb355afdf68d542
Rats <juliocesarmmc@hotmail.com>
2019-05-16 06:06:54 -0300

    First Commit

---
+++ src/dns.h
@@ -0,0 +1,49 @@
+/****************************************************************************
+ *                   ^     +----- |  / ^     ^ |     | +-\                  *
+ *                  / \    |      | /  |\   /| |     | |  \                 *
+ *                 /   \   +---   |<   | \ / | |     | |  |                 *
+ *                /-----\  |      | \  |  v  | |     | |  /                 *
+ *               /       \ |      |  \ |     | +-----+ +-/                  *
+ ****************************************************************************
+ * AFKMud Copyright 1997-2007 by Roger Libiez (Samson),                     *
+ * Levi Beckerson (Whir), Michael Ward (Tarl), Erik Wolfe (Dwip),           *
+ * Cameron Carroll (Cam), Cyberfox, Karangi, Rathian, Raine,                *
+ * Xorith, and Adjani.                                                      *
+ * All Rights Reserved.                                                     *
+ *                                                                          *
+ * External contributions from Remcon, Quixadhal, Zarius, and many others.  *
+ *                                                                          *
+ * Original SMAUG 1.8b written by Thoric (Derek Snider) with Altrag,        *
+ * Blodkai, Haus, Narn, Scryn, Swordbearer, Tricops, Gorog, Rennard,        *
+ * Grishnakh, Fireblade, Edmond, Conran, and Nivek.                         *
+ *                                                                          *
+ * Original MERC 2.1 code by Hatchet, Furey, and Kahn.                      *
+ *                                                                          *
+ * Original DikuMUD code by: Hans Staerfeldt, Katja Nyboe, Tom Madsen,      *
+ * Michael Seifert, and Sebastian Hammer.                                   *
+ ****************************************************************************
+ *                          DNS Resolver Module                             *
+ ****************************************************************************/
+
+#define DNS_FILE SYSTEM_DIR "dns.dat"
+
+typedef struct dns_data DNS_DATA;
+
+extern DNS_DATA *first_cache;
+extern DNS_DATA *last_cache;
+
+struct dns_data
+{
+   DNS_DATA *next;
+   DNS_DATA *prev;
+   const char *ip;
+   const char *name;
+   time_t time;
+};
+
+DECLARE_DO_FUN( do_cache );
+void resolve_dns( DESCRIPTOR_DATA * d, long ip );
+void process_dns( DESCRIPTOR_DATA * d );
+char *in_dns_cache( char *ip );
+void load_dns( void );
+void check_dns( void );
